38207f636730d3784319e782ade536f36704672f
Eric Snyder <ersnyder1@gmail.com>
2013-11-06 22:00:25 -0600

    Completed Week 6 HW
    App now contains functionality laid out in week #6 homework

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,48 @@
 class PicturesController < ApplicationController
   def show
+  	@id = params[:id]
+  	@p = Picture.find(@id)
   end
+
+  def index
+  	@list_of_pictures = Picture.all
+  end
+
+  def new
+  end
+
+  def create
+  	@picture = Picture.new
+	@picture.caption = params[:caption]
+	@picture.source = params[:source]
+	@picture.save
+  end
+
+  def destroy
+  	@id = params[:id]
+  	@pic_to_delete = Picture.find(@id)
+  	@pic_to_delete.destroy
+  end
+
+def edit
+	@id = params[:id]
+	@p = Picture.find(@id)
+	@picture_caption = @p.caption
+	@picture_source = @p.source
+end
+
+def update
+@id = params[:id]
+@p = Picture.find(@id)
+@p.caption= params[:caption] 
+@p.source= params[:source]
+@p.save
+end
+
+
+
+
+
+
+
 end
